74b50caf8c6e1e98786ce29b5b642c5b8deaac09
subwayno4 <subwayno4@gmail.com>
2021-07-23 11:22:20 +0900

    2021.07.23.v1

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
+import {Text} from './text.js';
+
 class app {
     constructor() {
-
+      this.setWebgl();
         WebFont.load({
             google: {
               families: ['Hind:700']
+            },
+            fontactive: () => {
+              this.Text = new Text();
+              this.Text.setText(
+                'A',
+                2,
+                document.body.clientWidth,
+                document.body.clientHeight,
+              );
             }
           });
-    }
+        }
+        setWebgl(){
+          this.renderer = new PIXI.Renderer({
+            width: document.body.clientWidth,
+            height: document.body.clientHeight,
+            antialias: true,
+            transparent: false,
+            resolution: (window.devicePixelRatio > 1) ? 2 : 1,
+            autoDensity: true,
+            powerPreference: "high-performance",
+            backgroundColor: 0xffffff,
+          });
+          document.body.appendChild(this.renderer.view);
+
+          this.stage = new PIXI.Container();
+        }
 }
 
 window.onload = () => {
